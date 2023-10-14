6aaa8da4464ce3cc6ab49930770319f7fa09b13a
Sofiamishel2003 <sofiamishel2003@gmail.com>
2023-10-13 19:38:49 -0600

    Update gráfico de Principal.kt y Receta.kta

--- app/src/main/java/com/example/easybites/Principal.kt
+++ app/src/main/java/com/example/easybites/Principal.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import coil.compose.rememberImagePainter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -72,13 +77,16 @@ fun ListaDeRecetasScreen(navController: NavHostController, recetas: List<Receta>
     LazyColumn(
         modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp) // Centrar tarjetas
     ) {
-        itemsIndexed(recetas) { index, receta ->
+        val chunkedRecetas = recetas.chunked(2) // Divide la lista en sublistas, cada una con 2 elementos como máximo
+        itemsIndexed(chunkedRecetas) { index, recetaPair ->
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                RecetaCard(receta = receta) {
-                    navController.navigate("Receta/$index")
+                for ((subIndex, receta) in recetaPair.withIndex()) {
+                    RecetaCard(receta = receta) {
+                        navController.navigate("Receta/${index * 2 + subIndex}")
+                    }
                 }
             }
         }
@@ -98,19 +106,25 @@ fun RecetaCard(receta: Receta, onClick: () -> Unit) {
         fontSize = 16.sp
     )
 
-    Card(onClick = onClick,
-        modifier = Modifier
-            .fillMaxWidth()
+    // Usar Box en lugar de Card
+    Box(
+        Modifier
+            .clickable(onClick = onClick)
+            .clip(RoundedCornerShape(8.dp))  // Esquinas redondas
+            .width(180.dp)
             .padding(8.dp)
-            .shadow(4.dp)
+            .shadow(0.dp)
     ) {
-        Column {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally  // Centrar texto horizontalmente
+
+        ) {
             val painter = rememberImagePainter(data = receta.imagenUrl)
             Image(
                 painter = painter,
                 contentDescription = null,
                 modifier = Modifier
-                    .height(150.dp)
+                    .height(140.dp)
                     .fillMaxWidth(),
                 contentScale = ContentScale.Crop
             )
@@ -127,6 +141,7 @@ fun RecetaCard(receta: Receta, onClick: () -> Unit) {
         }
     }
 }
+
 
 
 @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/com/example/easybites/Receta.kt
+++ app/src/main/java/com/example/easybites/Receta.kt
@@ -1,7 +1,10 @@
 package com.example.easybites
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,10 +13,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
 
@@ -22,13 +28,16 @@ fun DetallesDeRecetaScreen(receta: Receta) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally  // Centra todo el contenido horizontalmente
     ) {
+        // Título grande
         Text(
             text = receta.titulo,
             style = MaterialTheme.typography.titleLarge,
             modifier = Modifier.padding(bottom = 16.dp)
         )
+        // Imagen
         val painter = rememberImagePainter(data = receta.imagenUrl)
         Image(
             painter = painter,
@@ -39,15 +48,41 @@ fun DetallesDeRecetaScreen(receta: Receta) {
                 .padding(bottom = 16.dp),
             contentScale = ContentScale.Crop
         )
+        // Listado de ingredientes
         Text(text = "Ingredientes:", style = MaterialTheme.typography.titleMedium)
         receta.ingredientes.forEach { ingrediente ->
-            Text(text = "- $ingrediente", style = MaterialTheme.typography.bodyMedium)
+            Row(
+                horizontalArrangement = Arrangement.Center,  // Centra la fila
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "- $ingrediente",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp),  // Aumentar el tamaño de la letra
+                    textAlign = TextAlign.Right,  // Justificar a la derecha
+                )
+            }
         }
-        Text(text = "Tiempo: ${receta.tiempo}", style = MaterialTheme.typography.bodyMedium)
-        Text(text = "Presupuesto: ${receta.presupuesto}", style = MaterialTheme.typography.bodyMedium)
-        Text(text = "Procedimiento: ${receta.procedimiento}", style = MaterialTheme.typography.bodyMedium)
+        // Espacio entre ingredientes y aproximados
+        Spacer(modifier = Modifier.height(16.dp))
+        // Tiempo y presupuesto
+        Text(
+            text = "Tiempo: ${receta.tiempo}",
+            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp)
+        )
+        Text(
+            text = "Presupuesto: ${receta.presupuesto}",
+            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        // Título y procedimiento
+        Text(text = "Procedimiento", style = MaterialTheme.typography.titleMedium)
+        Text(
+            text = receta.procedimiento,
+            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp)
+        )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewDetallesDeRecetaScreen() {
