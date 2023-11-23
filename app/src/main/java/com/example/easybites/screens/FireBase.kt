aa9b47d6b11778c77ffb24a3cb47698602df838f
Riv2oo4 <riv22500@uvg.edu.gt>
2023-11-22 21:48:41 -0600

    Logo

--- app/src/main/java/com/example/easybites/screens/FireBase.kt
+++ app/src/main/java/com/example/easybites/screens/FireBase.kt
@@ -1,4 +1,5 @@
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.easybites.screens.Receta
@@ -8,7 +9,10 @@ import kotlinx.coroutines.tasks.await
 
 class MyViewModel : ViewModel() {
     private val db = FirebaseFirestore.getInstance()
-    var recetas = mutableListOf<Receta>()
+
+    // Utiliza MutableStateList en lugar de MutableList
+    var recetas = mutableStateListOf<Receta>()
+        private set
 
     init {
         obtenerRecetas()
@@ -17,22 +21,24 @@ class MyViewModel : ViewModel() {
     private fun obtenerRecetas() {
         val documentId = "8C6m2qCT2WtQ76XK3eD2"
         val documentId2 = "E4JNTtRZyC1bW6hu20G5"
+        val documentoId3 = "Wj7gPiY6ZxFvQL9jorz0"
+        val documentId4 = "egJaH73o2KtUNnTgKFdf"
 
         viewModelScope.launch {
             try {
-                val documento = db.collection("recetas").document(documentId2).get().await()
+                val documento = db.collection("recetas").document(documentId).get().await()
 
                 if (documento.exists()) {
                     val imagenUrl: String = documento.getString("imagenUrl") ?: ""
-                    val ingredientes: List<String> = documento.get("ingredientes") as? List<String> ?: emptyList()
+                    val ingredientes: List<String> =
+                        documento.get("ingredientes") as? List<String> ?: emptyList()
                     val presupuesto: String = documento.getString("presupuesto") ?: ""
                     val procedimiento: String = documento.getString("procedimiento") ?: ""
                     val tiempo: String = documento.getString("tiempo") ?: ""
                     val titulo: String = documento.getString("titulo") ?: ""
 
-                    // Actualizar la lista mutable con la nueva Receta
-                    recetas.clear()
-                    recetas.add(Receta(titulo, imagenUrl, ingredientes, tiempo, presupuesto, procedimiento))
+                    // Utiliza addAll en lugar de clear y add
+                    recetas.addAll(listOf(Receta(titulo, imagenUrl, ingredientes, tiempo, presupuesto, procedimiento)))
 
                     // Imprimir la lista de Recetas en el log
                     recetas.forEach { receta ->
@@ -46,6 +52,89 @@ class MyViewModel : ViewModel() {
                 Log.e("Error", "Error al obtener los datos", e)
             }
 
+            // Repite el mismo proceso para el segundo documento
+            try {
+                val documento = db.collection("recetas").document(documentId2).get().await()
+
+                if (documento.exists()) {
+                    val imagenUrl: String = documento.getString("imagenUrl") ?: ""
+                    val ingredientes: List<String> =
+                        documento.get("ingredientes") as? List<String> ?: emptyList()
+                    val presupuesto: String = documento.getString("presupuesto") ?: ""
+                    val procedimiento: String = documento.getString("procedimiento") ?: ""
+                    val tiempo: String = documento.getString("tiempo") ?: ""
+                    val titulo: String = documento.getString("titulo") ?: ""
+
+                    // Utiliza addAll en lugar de clear y add
+                    recetas.addAll(listOf(Receta(titulo, imagenUrl, ingredientes, tiempo, presupuesto, procedimiento)))
+
+                    // Imprimir la lista de Recetas en el log
+                    recetas.forEach { receta ->
+                        Log.d("Datos doc: ", "${receta.titulo}, ${receta.imagenUrl}")
+                    }
+                } else {
+                    Log.d("Error", "No existe el documento")
+                }
+            } catch (e: Exception) {
+                // Manejar errores aquí
+                Log.e("Error", "Error al obtener los datos", e)
+            }
+            try {
+                val documento = db.collection("recetas").document(documentoId3).get().await()
+
+                if (documento.exists()) {
+                    val imagenUrl: String = documento.getString("imagenUrl") ?: ""
+                    val ingredientes: List<String> =
+                        documento.get("ingredientes") as? List<String> ?: emptyList()
+                    val presupuesto: String = documento.getString("presupuesto") ?: ""
+                    val procedimiento: String = documento.getString("procedimiento") ?: ""
+                    val tiempo: String = documento.getString("tiempo") ?: ""
+                    val titulo: String = documento.getString("titulo") ?: ""
+
+                    // Utiliza addAll en lugar de clear y add
+                    recetas.addAll(listOf(Receta(titulo, imagenUrl, ingredientes, tiempo, presupuesto, procedimiento)))
+
+                    // Imprimir la lista de Recetas en el log
+                    recetas.forEach { receta ->
+                        Log.d("Datos doc: ", "${receta.titulo}, ${receta.imagenUrl}")
+                    }
+                } else {
+                    Log.d("Error", "No existe el documento")
+                }
+            } catch (e: Exception) {
+                // Manejar errores aquí
+                Log.e("Error", "Error al obtener los datos", e)
+            }
+            try {
+                val documento = db.collection("recetas").document(documentId4).get().await()
+
+                if (documento.exists()) {
+                    val imagenUrl: String = documento.getString("imagenUrl") ?: ""
+                    val ingredientes: List<String> =
+                        documento.get("ingredientes") as? List<String> ?: emptyList()
+                    val presupuesto: String = documento.getString("presupuesto") ?: ""
+                    val procedimiento: String = documento.getString("procedimiento") ?: ""
+                    val tiempo: String = documento.getString("tiempo") ?: ""
+                    val titulo: String = documento.getString("titulo") ?: ""
+
+                    // Utiliza addAll en lugar de clear y add
+                    recetas.addAll(listOf(Receta(titulo, imagenUrl, ingredientes, tiempo, presupuesto, procedimiento)))
+
+                    // Imprimir la lista de Recetas en el log
+                    recetas.forEach { receta ->
+                        Log.d("Datos doc: ", "${receta.titulo}, ${receta.imagenUrl}")
+                    }
+                } else {
+                    Log.d("Error", "No existe el documento")
+                }
+            } catch (e: Exception) {
+                // Manejar errores aquí
+                Log.e("Error", "Error al obtener los datos", e)
+            }
+
+
         }
     }
+
 }
+
